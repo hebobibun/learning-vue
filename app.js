b2599d143a6b784f21ca7d3b52689f471fe69f1c
Muhammad Habibullah <hebobibun@gmail.com>
2023-03-19 20:34:38 +0700

    assignment

--- app.js
+++ app.js
@@ -1,82 +1,34 @@
 const app = Vue.createApp({
   data() {
     return {
-      counter: 10,
-      name: '',
-      lastName: '',
-      // fullname: '',
-      // confirmedName: '',
-      age: ''
+      number: 0
     };
   },
-
-  watch: {
-    counter(value) {
-      if (this.counter > 50) {
-        const that = this
-        setTimeout(function(){
-          that.counter = 0;
-        }, 500)
+  computed: {
+    result() {
+      if (this.number < 37) {
+        return 'Not there yet!';
+      } else if (this.number === 37) {
+        return this.number;
+      } else {
+        return 'Too much!';
       }
     }
-    // name(value) {
-    //   if (value === '') {
-    //     this.fullname = '';
-    //   } else {
-    //     this.fullname = value + ' ' + this.lastName
-    //   }
-    // },
-    // lastName(value) {
-    //   if (value === '') {
-    //     this.fullname = '';
-    //   } else {
-    //     this.fullname = this.name + ' ' + value
-    //   }
-    // }
   },
-
-  computed: {
-    fullname() {
-      if (this.name === '' || this.lastName === '') {
-        return '';
-      } else {
-        return this.name + ' ' + this.lastName;
-      }
-    },
+  watch: {
+    result() {
+      console.log('Watcher executing...');
+      const that = this;
+      setTimeout(function() {
+        that.number = 0;
+      }, 5000);
+    }
   },
-  
   methods: {
-    submitForm(e) {
-      alert('Successfully submitted!');
-    },
-    // confirm() {
-    //   console.log('confirm...')
-    //   this.confirmedName = this.name;
-    // },
-    resetInput() {
-      this.name = '';
-      this.lastName = '';
-      this.fullname = '';
-      // this.confirmedName = '';
-      console.log('resetInput...')
-    },
-    resetCounter() {
-      this.counter = 10;
-      console.log('resetCounter...')
-    },
-    add(num) {
-      this.counter = this.counter + num;
-    },
-    reduce(num) {
-      if (this.counter >= num) {
-        this.counter = this.counter - num;
-      }
-    },
-    setAge(e, tahun) {
-      this.age = e.target.value + ' ' + tahun;
-      console.log(e)
-    },
+    addNumber(num) {
+      this.number = this.number + num;
+    }
   }
 });
 
-app.mount('#events');
+app.mount('#assignment');
